caa9e93e79ed3df0f4f2018933fe9d0560f5ab55
jcolber <jcolber@gmail.com>
2017-01-24 14:40:41 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -68,14 +68,15 @@
                             <div class="col-lg-12">
                                     <hr>
                                     <h2 class="intro-text text-center"><a href="services.php">Services
-                                        <strong>What we offer</strong></a>
+                                        <strong>What we offer</strong>
+                                        </a>
                                     </h2>
                                     <hr>
                                     <img class="img-responsive img-border img-left" src="img/offtrack.jpg" alt="Crooked Garage Door. Broken Garage Door Cable. Garage Door Repair">
                                     <hr class="visible-xs">
                                     <p>We at Salt City Garage Doors, believe that making your garage door safe is as important as making it function. For this reason, all of our garage door repairs and tune-ups feature a complete safety check. 
-                                    Our service technicians will carefully examine your garage door springs, safety sensors, hardware, track, and more. <p>This ensures that our work been done well, and that your garage door poses no danger to your family or property. We will always lubricate rollers, hinges, bearings and springs. We'll also adjust garage springs to balance your door and make sure your garage door openers safety reverse system is working. <p>Due to the intricacy of garage door systems, only garage service professionals
-                                    should attempt to adjust, repair, or service door equipment. Call us today for fast friendly service!<a href="tel:1-801-888-4359" class="linked-phone-number">  801.888.4359</p></a>
+                                    Our service technicians will carefully examine your garage door springs, safety sensors, hardware, track, and more. </p>This ensures that our work been done well, and that your garage door poses no danger to your family or property. We will always lubricate rollers, hinges, bearings and springs. We'll also adjust garage springs to balance your door and make sure your garage door openers safety reverse system is working. <p>Due to the intricacy of garage door systems, only garage service professionals
+                                    should attempt to adjust, repair, or service door equipment. Call us today for fast friendly service!<a href="tel:1-801-888-4359" class="linked-phone-number"> 801.888.4359</a></p>
                             </div>
                         </div>
                     </div>
@@ -85,11 +86,11 @@
                             <div class="col-lg-12">
                                 <hr>
                                 <h2 class="intro-text text-center"><a href="about.php">About Us
-                                    <strong>Why we do what we do</strong>
+                                    <strong>Why we do what we do</strong></a>
                                 </h2>
                                 <hr>
                                 <p>They say, you should always do what you love and next to my family, We love to fix things and make people happy.</p>
-                                <p>Our promise is to try and fix what others so quickly want to replace and charge more for. We also believe in only selling quality products that we would only install in our own homes. You can always expect us to arrive in the given time frame, as well as receive a phone call 15 minutes before our arrival.</p></a>
+                                <p>Our promise is to try and fix what others so quickly want to replace and charge more for. We also believe in only selling quality products that we would only install in our own homes. You can always expect us to arrive in the given time frame, as well as receive a phone call 15 minutes before our arrival.</p>
                             </div>
                         </div>
                     </div>
